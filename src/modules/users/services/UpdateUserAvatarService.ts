6fb492388ed9e269485a98237a14e9f89225b644
dev <tuco.anderson@gmail.com>
2020-05-15 17:45:49 -0300

    corrigindo imports DDD

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,9 +1,9 @@
 import path from 'path';
 import fs from 'fs';
 import { getRepository } from 'typeorm';
-import User from '../models/User';
-import uploadConfig from '../config/upload';
-import AppError from '../error/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/error/AppError';
 
 interface Request {
   user_id: string;
