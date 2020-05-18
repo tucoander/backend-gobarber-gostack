5b02d4e5b833178c006e1948bfda92d08309a225
dev <tuco.anderson@gmail.com>
2020-05-18 14:06:43 -0300

    criando/registrando container de users

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,17 @@ import {container} from 'tsyringe';
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+import UserRepositories from '@modules/users/infra/typeorm/repositories/UserRepositories';
+
+
 container.registerSingleton<IAppointmentRepository>(
   'AppointmentsRepository', 
   AppointmentsRepository
   );
+
+  container.registerSingleton<IUserRepository>(
+    'UserRepositories', 
+    UserRepositories
+    );
 
