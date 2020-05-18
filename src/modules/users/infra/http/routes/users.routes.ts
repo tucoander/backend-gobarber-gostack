72b50ccd6b7491fd62af6d5d021cd4ec67645318
dev <tuco.anderson@gmail.com>
2020-05-18 14:13:24 -0300

    ajustando rotas users com injecao de dependencia

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { container } from 'tsyringe';
 import multer from 'multer';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepositories';
 import CreateUserService from '@modules/users/services/CreateUserService';
@@ -13,8 +14,7 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
-    const usersRepository = new UsersRepository();
-    const createUser = new CreateUserService(usersRepository);
+    const createUser = container.resolve(CreateUserService);
 
     const user = await createUser.execute({
       name,
@@ -34,9 +34,8 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    // try {
-    const usersRepository = new UsersRepository();
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+   
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
@@ -46,9 +45,7 @@ usersRouter.patch(
     delete user.password;
 
     return response.json(user);
-    // } catch (err) {
-    //   return response.status(400).json({ error: err.message });
-    // }
+    
   },
 );
 
