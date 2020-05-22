306743dc172cae53ab7bd25f5aed9eeff8c60bea
Anderson Martins <tuco.anderson@gmail.com>
2020-05-22 17:33:58 -0300

    criando teste de logon sem usuario

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -29,5 +29,24 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+    
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@rocketseat.com',
+        password: '123456'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  
+    
+  });
   
 });
