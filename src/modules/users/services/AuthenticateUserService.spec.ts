2eee90e7b2d61157509ad3c4d81af211cca8daa8
Anderson Martins <tuco.anderson@gmail.com>
2020-05-22 17:39:45 -0300

    criação teste de logar com senha errada

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -11,19 +11,22 @@ describe('AuthenticateUser', () => {
 
     const authenticateUser = new AuthenticateUserService(
       fakeUserRepository,
-      fakeHashProvider
+      fakeHashProvider,
     );
-    const createUser = new CreateUserService(fakeUserRepository, fakeHashProvider);
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@rocketseat.com',
-      password: '123456'
+      password: '123456',
     });
 
     const response = await authenticateUser.execute({
       email: 'johndoe@rocketseat.com',
-      password: '123456'
+      password: '123456',
     });
 
     expect(response).toHaveProperty('token');
@@ -36,17 +39,41 @@ describe('AuthenticateUser', () => {
 
     const authenticateUser = new AuthenticateUserService(
       fakeUserRepository,
-      fakeHashProvider
+      fakeHashProvider,
     );
-    
+
     expect(
       authenticateUser.execute({
         email: 'johndoe@rocketseat.com',
-        password: '123456'
+        password: '123456',
       }),
     ).rejects.toBeInstanceOf(AppError);
-  
-    
   });
-  
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+   await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@rocketseat.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@rocketseat.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
